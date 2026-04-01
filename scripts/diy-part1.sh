242000552ded9ab4bcd8594228dac9b420f78da6
markw11 <mark.wang1@gmail.com>
2026-04-02 04:53:15 +0800

    Update diy-part1.sh

--- scripts/diy-part1.sh
+++ scripts/diy-part1.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 # diy-part1.sh
-opkg update
-opkg install kmod-batman-adv
+#opkg update
+#opkg install kmod-batman-adv
+
+sed -i '$a src-git batman https://github.com/open-mesh-mirror/batman-adv' feeds.conf.default
